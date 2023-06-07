e4a459260eb7499464d1f360e5fa8145d43f353d
Robbani571 <gr77rubel@gmail.com>
2023-06-07 09:56:33 +0600

    project file is ready to rock

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-bold text-purple-600 text-center">Wellcome to Artistry Moth School</h1>
+    </>
+  )
+}
+
+export default App
